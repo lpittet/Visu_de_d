5377270f98af506a70b2826dbc733e8de50a6467
lpittet <81432942+lpittet@users.noreply.github.com>
2021-04-10 16:17:06 +0200

    màj

---
+++ script.js
@@ -0,0 +1,25 @@
+let donnees = [30,60,120,340];
+
+let corps = d3.select('body');
+
+// ajouter un canevas à la page
+let canevas = corps.append('svg')
+    .attr('width',600)
+    .attr('height',600);
+
+// ajout d'un cercle
+// let mon_cercle = canevas.append('circle')
+//    .attr('cx',60)
+//    .attr('cy',60)
+//    .attr('r',40)
+
+// ajout d'autant de cercles qu'il y a de données
+
+canevas.selectAll('circle')
+    .data(donnees)
+    .enter()
+    .append('circle')
+        .attr('cx',d => d)
+        .attr('cy',300)
+        .attr('r',(d,i) => i*20)
+        .style('opacity',0.5);
