cdda36334a2d32a9e5f4c930cd91bca55d1d8b1c
lpittet <81432942+lpittet@users.noreply.github.com>
2021-04-10 16:53:30 +0200

    màj3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.write(d3.version);
+
 let donnees = [30,60,120,340];
 
 let corps = d3.select('body');
@@ -21,5 +23,5 @@ canevas.selectAll('circle')
     .append('circle')
         .attr('cx',d => d)
         .attr('cy',300)
-        .attr('r',(d,i) => i*20)
+        .attr('r',(d,i) => 5+(i*20))
         .style('opacity',0.5);
